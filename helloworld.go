09bb03f37756a4b46d65e773789ed2b57689d693
amhaske <amhaske32@gmail.com>
2022-05-21 19:42:55 -0400

    chore: Add helloworld.go

---
+++ helloworld.go
@@ -0,0 +1,9 @@
+// Package fmt implements formatted I/O with functions analogous to C's printf and scanf.
+// The format 'verbs' are derived from C's but are simpler.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
